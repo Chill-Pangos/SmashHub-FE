7b7b4b5eda34d8d6323737d9ed87f066fb8c67c1
Minh Tuấn <75988895+MinhTuan120704@users.noreply.github.com>
2025-10-01 02:25:27 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <div className="dark">
+      <div className="bg-white dark:bg-gray-800 text-black dark:text-white">
+        Hello Dark Mode
+      </div>
+    </div>
+  );
+}
+
+export default App
